a6bccb588ee6515044e9058779c58c3502fc044d
szhokee <szhokee@gmail.com>
2023-04-12 21:57:23 +0600

    mov1

---
+++ main/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Movie API',
+        default_version = 'v1',
+        description = 'Web-Site'
+    ),
+    public=True
+)
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger')),
+    path('account/', include('account.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
